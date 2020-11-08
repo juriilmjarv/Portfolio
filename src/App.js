36145fff46d56811c42f7199b3256f04b3619cb3
Juri Ilmjarv <juriilmjarv@juris-mbp.lan>
2020-11-08 22:42:48 +0000

    projects page layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
 import Main from './components/main';
 import {Link} from 'react-router-dom';
-
+import {Navbar, Nav} from 'react-bootstrap'
 function App() {
   return (
     
     <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      {/*
       <Layout fixedHeader>
           <Header style={{backgroundColor: 'transparent', zIndex: '1'}}className='header-color' title={<a href="/" style={{textDecoration:'none'}}><strong>Juri ilmjarv</strong></a>}>
               <Navigation>
@@ -28,6 +29,20 @@ function App() {
             <Main/>
           </Content>
       </Layout>
+      */}
+      <Navbar bg="dark" expand="lg" className='header-color'>
+        <Navbar.Brand href="/">Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/resume">Resume</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Main/>
     </div>
   );
 }
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
-import {Grid, Cell } from 'react-mdl';
 import profilepic from '../assets/pictures/DSC01233.jpg';
+import {Container, Row, Col, Image} from 'react-bootstrap';
 
 class Landing extends Component {
     render(){
         return(
-            <div style={{width: '100%', margin: 'auto'}}>
+            <div className="landing-grid">
+                <Container >
+                    <Row>
+                        <Col md={{ span: 6, offset: 3 }}><Image className='circular--portrait'src={profilepic} roundedCircle></Image></Col>
+                    </Row>
+                    <Row>
+                    <div className="banner-text">
+                            <h1>Software Developer</h1>
+                            <hr/>
+                            <p>HTML/CSS | Bootstrap | JavaScript | React | React Native</p>
+
+                            <div className="social-links">
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                </a>
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true"/>
+                                </a>
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                </a>
+                            </div>
+                        </div>
+                    </Row>
+                </Container>
+                {/*
                 <Grid className="landing-grid">
                     <Cell col={12}>
                         <div className='circular--portrait'>
@@ -36,6 +61,7 @@ class Landing extends Component {
                     </Cell>
 
                 </Grid>
+                */}
             </div>
         )
     }
